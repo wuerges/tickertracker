9d57bebfa5b026448e8395f7a0f21cb4d2cd1230
Emilio Wuerges <wuerges@gmail.com>
2021-10-11 16:14:07 -0300

    created view for user

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -6,4 +6,12 @@ class Ledger < ApplicationRecord
   def balance
     self.orders.map {|o| o.amount }.sum
   end
+
+  def total_value
+    self.orders.map {|o| o.amount * o.price  }.sum
+  end
+
+  def average_price
+    self.total_value.to_f / self.balance
+  end
 end
