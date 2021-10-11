c3b0c01fc8f2aa3278b5a1454e4a00c5f0682aa1
Emilio Wuerges <wuerges@gmail.com>
2021-10-11 15:54:02 -0300

    Added seeds to help development

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,14 @@
 
 case Rails.env
 when "development"
-    user = User.create(login: "Jack Boggle")
-    ticker = Ticker.create(ticker: "BTCUSD", 
-        description: "BTC/USD from Coinbase", 
-        url:"https://api.coinbase.com/v2/prices/BTC-USD/buy",
-        precision: 2
-    )
-    ticker.fetch_price!
-    user.ledgers.create(ticker: ticker)
+  user = User.create(login: "Jack Boggle")
+  ticker = Ticker.create(ticker: "BTCUSD",
+                         description: "BTC/USD from Coinbase",
+                         url: "https://api.coinbase.com/v2/prices/BTC-USD/buy",
+                         precision: 2)
+  ticker.fetch_price!
+  btc_ledger = user.ledgers.create(ticker: ticker)
+  btc_ledger.orders.create(amount: 1, price: 10)
+  btc_ledger.orders.create(amount: 2, price: 5)
+  btc_ledger.orders.create(amount: 3, price: 1)
 end
