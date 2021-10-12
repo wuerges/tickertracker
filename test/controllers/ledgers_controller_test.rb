f61d141fc0fc0f4cb9f86104832899d697378af0
Emilio Wuerges <wuerges@gmail.com>
2021-10-12 10:42:49 -0300

    Removed tests from ledgers controller

--- test/controllers/ledgers_controller_test.rb
+++ test/controllers/ledgers_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class LedgersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get ledgers_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get ledgers_show_url
-    assert_response :success
-  end
 end
