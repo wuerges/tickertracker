1ef3f1dd5aaf8608144ff2baf2d45cf16b99048d
Emilio Wuerges <wuerges@gmail.com>
2021-10-12 10:42:09 -0300

    Removed useless coments from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # get 'orders/create'
-  # get 'orders/destroy'
-  # get 'ledgers/index'
-  # get 'ledgers/show'
   resources :ledgers, only: [] do
     resources :orders, only: [:create, :destroy]
   end
@@ -12,6 +8,4 @@ Rails.application.routes.draw do
       post 'fetch_price'
     end
   end
-  #post '/tickers/:id/fetch', to: 'tickers#fetch', as: 'ticker_fetch'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
